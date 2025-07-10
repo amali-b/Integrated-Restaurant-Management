bce9259e3fe50afc2a87629294fa5ffaa15ed436
Amali <amalibhuvisara@gmail.com>
2025-07-11 01:21:56 +0530

    write : query to get batchnumber if not exist

--- src/main/java/lk/restaurant_management/dao/InventoryDao.java
+++ src/main/java/lk/restaurant_management/dao/InventoryDao.java
@@ -1,9 +1,17 @@
 package lk.restaurant_management.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import lk.restaurant_management.entity.Inventory;
 
 public interface InventoryDao extends JpaRepository<Inventory, Integer> {
 
+    // get next batchnumber
+    @Query(value = "SELECT concat('BNO', lpad(substring(max(inv.batchnumber),4)+1,4,0)) FROM resturant_management_project.inventory as inv where inv.batchnumber like'BNO%';", nativeQuery = true)
+    String getNextBatchNumber();
+
+    // inventory eken thyena batchnumber ekai ingredient ekai aregnnawa
+    @Query(value = "SELECT * FROM resturant_management_project.inventory as inv where inv.ingredient_id=?1 and inv.batchnumber=?2", nativeQuery = true)
+    Inventory getByIngredientBatchNumber(Integer id, String batchnumber);
 }
